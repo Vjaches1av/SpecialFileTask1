6f7a75416b04682c25c87a6add6395a51ec8d592
Vyacheslav <informationsss@ya.ru>
2022-03-06 17:29:45 +0300

    Задача выполнена

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVParser;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
@@ -7,13 +9,14 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Scanner;
 
 public final class Main {
 
-    private static <T> List<T> csvToObj(File file, char separator, Class<T> type, String... classFields) throws IOException {
+    private static <T> List<T> csvToListObj(File file, char separator, Class<T> type, String... classFields) throws IOException {
         CSVParser parser = new CSVParserBuilder()
                 .withSeparator(separator)
                 .build();
@@ -30,26 +33,28 @@ public final class Main {
         }
     }
 
+    private static <T> String listObjToJson(List<T> list) {
+        Type listType = new TypeToken<List<T>>() {
+        }.getType();
+        return new Gson().toJson(list, listType);
+    }
+
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                System.out.print("Укажите путь к файлу *.csv, включая диск: ");
-                File file = new File(scanner.nextLine());
+            System.out.print("Укажите путь к файлу *.csv, включая диск: ");
+            File file = new File(scanner.nextLine());
+            try {
                 if (file.exists() && file.isFile() && file.getName().endsWith(".csv")) {
-                    try {
-                        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-                        List<Employee> employees = csvToObj(file, ';', Employee.class, columnMapping);
-                        System.out.println(employees);
-
-                        return;
-                    } catch (FileNotFoundException e) {
-                        System.err.println("Проверьте правильность указания пути и имени файла.");
-                    } catch (IOException e) {
-                        System.err.println("Произошла ошибка при попытке чтения указанного файла.");
-                    }
+                    String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+                    List<Employee> employees = csvToListObj(file, ';', Employee.class, columnMapping);
+                    System.out.println(listObjToJson(employees));
                 } else {
-                    System.err.println("Проверьте правильность указания пути и имени файла.");
+                    throw new FileNotFoundException();
                 }
+            } catch (FileNotFoundException e) {
+                System.err.println("Проверьте правильность указания пути и имени файла.");
+            } catch (IOException e) {
+                System.err.println("Произошла ошибка при попытке чтения указанного файла.");
             }
         }
     }
